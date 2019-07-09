9e770cea6c38e4baec7e529c7a0d44ca1aa81927
Martin Rocek <silence.sys@gmail.com>
2019-07-09 18:08:02 +0100

    :sparkles: Initial commit, yay!

---
+++ components/Canvas.vue
@@ -0,0 +1,15 @@
+<template>
+    <div />
+</template>
+
+<script>
+export default {
+    name: 'QUIT--Canvas',
+    props: {},
+    data() {},
+    computed: {},
+    watch: {},
+    mounted() {},
+    methods: {}
+}
+</script>
